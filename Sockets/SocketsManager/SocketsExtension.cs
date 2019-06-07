5f9bf4ead1d7e409ca01400e911befa5b478868e
Can Özaytekin <canozaytekin@hotmail.com>
2019-06-07 09:31:42 +0100

    Add project files.

---
+++ Sockets/SocketsManager/SocketsExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebSockets;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sockets.SocketsManager
+{
+    public static class SocketsExtension
+    {
+        public static IServiceCollection AddWebSocketManager(this IServiceCollection services)
+        {
+            services.AddTransient<ConnectionManager>();
+            foreach (var type in Assembly.GetEntryAssembly().ExportedTypes)
+                if (type.GetTypeInfo().BaseType == typeof(SocketHandler))
+                    services.AddSingleton(type);
+            return services;
+        }
+
+        public static IApplicationBuilder MapSockets(this IApplicationBuilder app, PathString path,
+            SocketHandler socket)
+        {
+            return app.Map(path, x => x.UseMiddleware<WebSocketMiddleware>(socket));
+        }
+    }
+}
---
+++ Sockets/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Sockets.Handlers;
+using Sockets.SocketsManager;
+
+namespace Sockets
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddWebSocketManager();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseWebSockets();
+            app.MapSockets("/ws", serviceProvider.GetService<WebSocketMessageHandler>());
+            app.UseStaticFiles();
+        }
+    }
+}
